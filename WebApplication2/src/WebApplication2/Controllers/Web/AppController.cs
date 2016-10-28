5de766ffce82ab68648c5eb968d39fc1eb93f8d4
fatih bulut <fatih.bulut@xoka.com>
2016-10-28 17:37:13 +0300

    newed

---
+++ WebApplication2/src/WebApplication2/Controllers/Web/AppController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApplication2.Services;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We dont support AOL Address!");
+            }
+            
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From WebApp", model.Message);
+            }
+
+            ModelState.Clear();
+
+            ViewBag.UserMessage = "Message Sent";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/src/WebApplication2/Services/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication2.Services
+{
+    public interface IMailService
+    {
+        void SendMail(string to, string from, string subject, string body);
+    }
+}
